bde5593ebe06081571593fb1094735a4df024e7b
eushakov@umg-marketing.com <eushakov@umg-marketing.com>
2021-04-19 17:18:00 +0700

    нормальный путь

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const config = require('../config/db.js');
+const path = require('path');
 const app = express();
+app.use(express.static(path.join(__dirname,"../build")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const uri = config.url;
@@ -12,7 +14,6 @@ mongoClient.connect((err, client) => {
 
   // Подрубаем роуты, пихаем туда экспресс объект для создания роутов и  монго объект для работы с бд
   require('./routes')(app, client);
-
   app.get('/', function (req, res) {
     res.send('Hello World')
   })
@@ -20,7 +21,6 @@ mongoClient.connect((err, client) => {
   app.listen(1234, () => {
     console.log('сервер поехал');
   })
-
   const collection = client.db("portal").collection("beer");
   // perform actions on the collection object
   // console.log(collection);
